8393518fed807b38df35289273d9138186d0cfc7
Benjamin Earley <bearley@outsitenetworks.com>
2015-10-11 15:34:30 -0400

    adds other apps

--- app/src/main/java/com/benjaminearley/portfolioapp/MainActivity.java
+++ app/src/main/java/com/benjaminearley/portfolioapp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.benjaminearley.portfolioapp;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -8,6 +10,8 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private PackageManager manager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,16 +19,53 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        manager = this.getPackageManager();
     }
 
     public void launchSpotifyStreamer(View view) {
-        Toast.makeText(this, "This button will launch my Spotify Streamer App!", Toast.LENGTH_LONG).show();
+
+        Intent i = manager.getLaunchIntentForPackage("com.benjaminearley.spotifystreamer");
+
+        if (i != null) {
+            i.addCategory(Intent.CATEGORY_LAUNCHER);
+        } else {
+            Toast.makeText(this, "This button should launch my Spotify Streamer App!", Toast.LENGTH_LONG).show();
+        }
+        try {
+            this.startActivity(i);
+        } catch (NullPointerException e) {
+            Toast.makeText(this, "This button should launch my Spotify Streamer App!", Toast.LENGTH_LONG).show();
+        }
+
     }
     public void launchScoresApp(View view) {
-        Toast.makeText(this, "This button will launch my Scores App!", Toast.LENGTH_LONG).show();
+        Intent i = manager.getLaunchIntentForPackage("com.benjaminearley.spotifystreamer");
+
+        if (i != null) {
+            i.addCategory(Intent.CATEGORY_LAUNCHER);
+        } else {
+            Toast.makeText(this, "This button should launch my Football Scores App!", Toast.LENGTH_LONG).show();
+        }
+        try {
+            this.startActivity(i);
+        } catch (NullPointerException e) {
+            Toast.makeText(this, "This button should launch my Football Scores App!", Toast.LENGTH_LONG).show();
+        }
     }
     public void launchLibraryApp(View view) {
-        Toast.makeText(this, "This button will launch my Library App!", Toast.LENGTH_LONG).show();
+        Intent i = manager.getLaunchIntentForPackage("com.benjaminearley.spotifystreamer");
+
+        if (i != null) {
+            i.addCategory(Intent.CATEGORY_LAUNCHER);
+        } else {
+            Toast.makeText(this, "This button should launch my Alexandria App!", Toast.LENGTH_LONG).show();
+        }
+        try {
+            this.startActivity(i);
+        } catch (NullPointerException e) {
+            Toast.makeText(this, "This button should launch my Alexandria App!", Toast.LENGTH_LONG).show();
+        }
     }
     public void launchBuildItBigger(View view) {
         Toast.makeText(this, "This button will launch my Build It Bigger App!", Toast.LENGTH_LONG).show();
